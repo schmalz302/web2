fad4d310c046a9145c2c733a2add7636e63e2cdc
buk55 <boltneffe@yandex.ru>
2021-03-16 16:42:31 +0500

    Автоматический ответ

--- server.py
+++ server.py
@@ -2,11 +2,18 @@ from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
+a = {'surname': 'Watny', 'name': 'Mark', 'education': 'выше среднего',
+        'profession': 'штурман марсохода', 'sex': 'male', 'motivation': 'Всегда мечтал застрять на Марсе!',
+        'ready': 'True'}
+b = ['surname', 'name', 'education', 'profession', 'sex', 'motivation', 'ready']
+c = ['Фамилия:', 'Имя:', 'Образование:', 'Профессия:', 'Пол:', 'Мотивация:', 'Готовы остаться на Марсе?']
 
-@app.route('/training/<prof>')
-def index(prof):
-    return render_template('специальность.html',
-                           a=prof)
+
+@app.route('/answer')
+@app.route('/auto_answer')
+def index():
+    return render_template('auto_answer.html',
+                           a=a, b=b, c=c)
 
 
 if __name__ == '__main__':
